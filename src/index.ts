d2e8ad5b55dbdde68ebf7e5a4bdae2b4fb48e512
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2018-11-04 13:51:51 +0700

    Implement iteration over object keys and entries
    `overKey()` and `overEntries()` functions.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './a-iterable';
 export * from './array-like-iterable';
 export * from './construction';
+export * from './objects';
 export * from './revertible-iterable';
 export * from './termination';
 export * from './transform';
---
+++ src/objects.ts
@@ -0,0 +1,44 @@
+import { overArray } from './construction';
+import { RevertibleIterable } from './revertible-iterable';
+import { mapIt } from './transform';
+
+/**
+ * Builds an iterable over the keys of the given object.
+ *
+ * A list of keys is constructed using `Reflect.ownKeys()`.
+ *
+ * @param target An object to select keys from.
+ */
+export function overKeys<T extends object>(target: T): RevertibleIterable<keyof T> {
+  return overArray(Reflect.ownKeys(target) as (keyof T)[]);
+}
+
+/**
+ * Object property entry. This is a tuple consisting of property key and value.
+ */
+export type ObjectEntry<T, K extends keyof T = keyof T> = [K, T[K]];
+
+/**
+ * Builds an iterable over the key/value entries of the given object.
+ *
+ * A list of keys is constructed using `Reflect.ownKeys()`.
+ *
+ * @param target An object to select keys and values from.
+ */
+export function overEntries<T extends object>(target: T): RevertibleIterable<ObjectEntry<T>> {
+
+  const keys = overKeys(target);
+
+  function mapToEntries(_keys: Iterable<keyof T>): Iterable<ObjectEntry<T>> {
+    return mapIt(_keys, key => [key, target[key]] as ObjectEntry<T>);
+  }
+
+  return {
+    [Symbol.iterator]() {
+      return mapToEntries(keys)[Symbol.iterator]();
+    },
+    reverse() {
+      return mapToEntries(keys.reverse());
+    },
+  };
+}
