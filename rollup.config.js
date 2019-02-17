82d7748d0246e018d05ee78553f4d1a7005bf124
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-02-17 12:39:05 +0700

    Do not generate browser bundle

--- rollup.config.js
+++ rollup.config.js
@@ -82,15 +82,6 @@ const mainConfig = makeConfig(
     },
     terserConfig);
 
-const umdConfig = makeConfig(
-    baseConfig('tsconfig.umd.json'),
-    {
-      output: {
-        file: pkg.browser,
-      },
-    },
-    uglifyConfig);
-
 const esmConfig = makeConfig(
     baseConfig('tsconfig.esm.json'),
     {
@@ -113,7 +104,6 @@ const esm5Config = makeConfig(
 
 export default [
   mainConfig,
-  umdConfig,
   esmConfig,
   esm5Config,
 ]
