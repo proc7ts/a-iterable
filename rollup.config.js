e0054a90b4951bcf7a4a71a681b3c512ed89acf7
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-02-17 12:42:48 +0700

    Generate esm bundles by default

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ const mainConfig = makeConfig(
     baseConfig('tsconfig.main.json'),
     {
       output: {
+        format: 'umd',
         file: pkg.main,
       },
     });
@@ -16,7 +17,6 @@ const esmConfig = makeConfig(
     baseConfig('tsconfig.esm.json'),
     {
       output: {
-        format: 'esm',
         file: pkg.module,
       },
     });
@@ -25,7 +25,6 @@ const esm5Config = makeConfig(
     baseConfig('tsconfig.umd.json'),
     {
       output: {
-        format: 'esm',
         file: pkg.esm5,
       },
     });
@@ -64,7 +63,7 @@ function baseConfig(tsconfig) {
       'tslib',
     ],
     output: {
-      format: 'umd',
+      format: 'esm',
       sourcemap: true,
       name: 'aIterable',
       globals: {
